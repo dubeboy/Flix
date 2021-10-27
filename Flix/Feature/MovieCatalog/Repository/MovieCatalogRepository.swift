d4216a998cdacb387dc976dcf0e85542ca7865fb
Divine Dube <dubedivine@gmail.com>
2021-10-28 01:58:08 +0400

    Init create movies service

--- Flix/Feature/MovieCatalog/Repository/MovieCatalogRepository.swift
+++ Flix/Feature/MovieCatalog/Repository/MovieCatalogRepository.swift
@@ -10,3 +10,9 @@ import Foundation
 protocol MovieCatalogRepository {
     func getMovies(result: @escaping NetworkResponseCompletion<Movie>)
 }
+
+class MovieCatalogRepositoryImpl: MovieCatalogRepository {
+    func getMovies(result: @escaping NetworkResponseCompletion<Movie>) {
+        
+    }
+}
---
+++ Flix/Service/FlixService.swift
@@ -0,0 +1,15 @@
+//
+//  FlixService.swift
+//  Flix
+//
+//  Created by Divine.Dube on 2021/10/28.
+//
+
+import Foundation
+import Merchant
+
+struct FlixService: Service {
+    var baseURL: String = ""
+    
+    
+}
