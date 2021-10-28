5ad99b9bea45320729c78997b1ca527d1ff309f1
Divine Dube <dubedivine@gmail.com>
2021-10-28 14:35:27 +0400

    Create Flix service

--- Flix/Service/FlixNetworkService.swift
+++ Flix/Service/FlixNetworkService.swift
@@ -10,6 +10,8 @@ import Merchant
 
 struct FlixNetworkService: Service {
     var baseURL: String = ""
+    var query = ["api_key" : "1b5b100291ca41efa1d85ad150587d86", "langauge": "en-US"]
     
-    
+    @GET("https://api.themoviedb.org/3/movie/upcoming")
+    var getUpcomingMovies: MoviePageModel
 }
