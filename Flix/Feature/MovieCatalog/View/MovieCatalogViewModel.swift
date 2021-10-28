c30c4492cc33d8b2b1bf196b211f782808675747
Divine Dube <dubedivine@gmail.com>
2021-10-28 15:24:37 +0400

    Add ViewModel Completion

--- Flix/Feature/MovieCatalog/View/MovieCatalogViewModel.swift
+++ Flix/Feature/MovieCatalog/View/MovieCatalogViewModel.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 protocol MovieCatalogViewModel {
-    func getUpComingMovies()
+    // We have two callbacks to the view
+    // The completion with the upcoming movies escaping because the callback is async
+    // The completion with error with a simple string that can be displayed on the screen
+    //
+    // We used two completion because we want the view to be a dumb and as linear as possible(not many if statments)
+    func getUpComingMovies(completion: @escaping Completion<[Movie]>, error: @escaping Completion<String>)
 }
 
 
--- Flix/Shared/View/Completion.swift
+++ Flix/Shared/View/Completion.swift
@@ -7,4 +7,6 @@
 
 import Foundation
 
+/// A Completion clousure - That take one generic paramenter and dons't return
+/// In the case that you don't want it to take a void in other words () -> Void you can pass () as the generic paramanter
 typealias Completion<T> = (T) -> Void
